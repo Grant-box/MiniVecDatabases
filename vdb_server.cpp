42c381757cef085ffb6a446589d543821f01c0c4
Grant-box <2414578614@qq.com>
2024-11-12 12:00:25 +0800

    update

--- vdb_server.cpp
+++ vdb_server.cpp
@@ -3,18 +3,30 @@
 #include "index_factory.h"
 
 int main(){
+    
+    // 初始化全局日志记录器
     init_global_logger();
+    // 设置日志级别为调试
     set_log_level(spdlog::level::debug);
 
+    // 记录服务器启动信息
     GlobalLogger->info("VDB Server is starting...");
 
+    // 设置索引的维度
     int dim = 1;
+    // 获取全局索引工厂
     IndexFactory* globalIndexFactory = getGlobalIndexFactory();
+    // 初始化索引工厂
     globalIndexFactory->init(IndexFactory::IndexType::FLAT, dim);
+    // 记录服务器初始化信息
     GlobalLogger->info("VDB Server is initialized");
     
+    // 创建HTTP服务器实例
     HttpServer http_server("localhost", 8080);
+    // 记录服务器运行信息
     GlobalLogger->info("VDB Server is running");
+    // 启动HTTP服务器
     http_server.start();
+    // 返回0表示程序正常结束
     return 0;
 }
